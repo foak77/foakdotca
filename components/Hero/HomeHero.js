5cf1e8e0e240bedddf170b12989c934888ccb4a2
foak77 <emailtofred@gmail.com>
2024-05-09 16:21:36 -0400

    index modification

--- components/Hero/HomeHero.js
+++ components/Hero/HomeHero.js
@@ -9,7 +9,11 @@ export default function Hero() {
       <Image className={styles.hero__toprip} src={toprip} alt="hero" />
       <section className={styles.hero__helloFrame}>
         <h1 className={styles.hero__title}>Welcome!</h1>
-        <p className={styles.hero__text}>Step Inside and Explore</p>
+        <h2 className={styles.hero__underTitle}>To my online resume</h2>
+        <p className={styles.hero__text}>- Step Inside and Explore -</p>
+        <p className={styles.hero__underText}>
+          I'm in pursuit of fresh career horizons!
+        </p>
       </section>
       <Image className={styles.hero__bottomrip} src={toprip} alt="hero" />
     </section>
