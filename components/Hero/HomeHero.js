d87becc4a147c8e0c151d3382798bbd502f987ce
foak77 <emailtofred@gmail.com>
2024-12-01 12:32:54 -0500

    new front hero background

--- components/Hero/HomeHero.js
+++ components/Hero/HomeHero.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "./../Hero/HomeHero.module.scss";
-// import heroImage from "./../../public/images/front-Image.jpg";
 import toprip from "./../../public/images/transition1.png";
 
 export default function Hero() {
@@ -17,16 +16,8 @@ export default function Hero() {
         className={`${styles.hero__vid}`}
         alt="video"
       >
-        <source src="/videos/front-hero.mp4" type="video/mp4" />
+        <source src="/videos/front-hero-1.mp4" type="video/mp4" />
       </video>
-      {/* <section className={styles.hero__helloFrame}>
-        <h1 className={styles.hero__title}>Welcome!</h1>
-        <h2 className={styles.hero__underTitle}>To my online resume</h2>
-        <p className={styles.hero__text}>- Step Inside and Explore -</p>
-        <p className={styles.hero__underText}>
-          I am in pursuit of fresh career horizons!
-        </p>
-      </section> */}
       <Image className={styles.hero__bottomrip} src={toprip} alt="hero" />
     </section>
   );
